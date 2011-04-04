ca6e4c1d338983ffdc5b2329ffc8f49507b881f6
lucas.uccello <lucas.uccello@ea5e0ef0-e43b-b66d-2e18-010731ec5258>
2011-04-04 13:55:37 +0000

    Handler del archivo de RLV terminado, ya hace, insertar, buscar y eliminar.

--- trunk/tpDatos2011/src/ManejadorArchivoRLV/HandlerArchivoRLV.cpp
+++ trunk/tpDatos2011/src/ManejadorArchivoRLV/HandlerArchivoRLV.cpp
@@ -14,7 +14,7 @@ int HandlerArchivoRLV::obtenerTamanioLibro(char * cadenaDeDatos)
 }
 
 
-void HandlerArchivoRLV::recuperarUltimoID(){
+void HandlerArchivoRLV::recuperarDatosAnteriores(){
 	FILE* arch;
 	if ((arch = fopen(PATH_CONFIG_RLV, "r")) == NULL) {
 		cout << "Error al intentar abrir el archivo: " << PATH_CONFIG_RLV << endl;
@@ -46,8 +46,7 @@ void HandlerArchivoRLV::recuperarUltimoID(){
 }
 
 HandlerArchivoRLV::HandlerArchivoRLV() {
-    recuperarUltimoID();
-
+    recuperarDatosAnteriores();
 }
 
 int HandlerArchivoRLV::insertarRegistro(const string& path_nuevo_libro)
@@ -64,67 +63,69 @@ int HandlerArchivoRLV::insertarRegistro(const string& path_nuevo_libro)
 	f_ent.close();
 
 
-// Obtengo ID del ultimo libro ingresado
+// Obtengo ID del ultimo libro ingresado y seteo todos sus datos
 	int id_Archivo = this->ultimoID + 1;
     this->ultimoID = id_Archivo;
-// Manejo sobre el archivo de RLV
     this->ultimoOffset = obtenerTamanioMaestro();
-    grabarUltimoID();
+
+    grabarDatosActuales();
 
     int indexado = 0;
 	int procesado = 1;
 
+// Manejo sobre el archivo de RLV
 	std::ofstream f_dst;
 	f_dst.open(PATH_REG_LONG_VARIABLE, std::ios_base::app);
 	stringstream ss;
-	// Armo los datos de entrada para ese libro
-	ss << id_Archivo << "|" << size << "|" << procesado << "|" << indexado << "|" << "\n" << buffer;
+// Armo las cabecera para el libro
+	ss << id_Archivo << "|" << size << "|" << procesado << "|" << indexado << "|" << "\n" << buffer << "\n" ;
 	string str = ss.str();
 	// Escribo libro en el archivo de RLV
 	f_dst.write(str.c_str(), str.length());
 	f_dst.close();
 	free(buffer);
+
 // Retorno el offset que se guardara en el arbol.
 	return this->ultimoOffset;
 }
 
-void HandlerArchivoRLV::grabarUltimoID(){
+void HandlerArchivoRLV::grabarDatosActuales(){
+
 	std::ofstream f_dst;
 	f_dst.open(PATH_CONFIG_RLV, std::ios_base::out);
 	stringstream ss;
-	// Armo los datos de entrada para ese libro
 	ss << this->ultimoID << "|" << this->ultimoOffset;
 	string str = ss.str();
-	// Escribo libro en el archivo de RLV
+
+	// Escribo libro en el archivo de control el ultimo offset y ID.
 	f_dst.write(str.c_str(), str.length());
 	f_dst.flush();
 	f_dst.close();
 }
 
-/*
- * Devuelve un puntero a una cadena de char que contiene todo el libro
- */
 char* HandlerArchivoRLV::buscarRegistro(int offset)
 {
 	std::ifstream archivoMaestro;
 	char  cadenaDeDatos[100];
 	archivoMaestro.open(PATH_REG_LONG_VARIABLE, std::ios_base::in);
+	// Me posiciono en el archivo de RLV
 	archivoMaestro.seekg(offset);
 
 	archivoMaestro.get(cadenaDeDatos,100);
 	string cad = cadenaDeDatos;
 	int longitudCadena = cad.length();
-
 	longitudCadena += offset;
-
 	//Obtengo el tamanio del libro a leer
 	int espacioOcupado = obtenerTamanioLibro(cadenaDeDatos);
+
 	/*Le sumo a la longitud de la cadena de datos el offset para posicionarme nuevamente
 	 en el archivo*/
 	archivoMaestro.seekg(longitudCadena);
+	// obtengo el libro
 	char * libroLeido = (char*)calloc (espacioOcupado, sizeof(char));
 	archivoMaestro.read(libroLeido, espacioOcupado);
 	archivoMaestro.close();
+
 	return libroLeido;
 }
 
@@ -139,4 +140,25 @@ int HandlerArchivoRLV::obtenerTamanioMaestro(){
 	rewind(fin);
 	fclose(fin);
 	return retorno;
+}
+
+void HandlerArchivoRLV::quitarRegistro(int offset){
+// Abro el archivo y me posiciono para obtener los datos
+	std::fstream fh;
+	char  cadenaDeDatos[100];
+	fh.open(PATH_REG_LONG_VARIABLE, std::ios_base::in | std::ios_base::out);
+	fh.seekg(offset);
+	fh.get(cadenaDeDatos,100);
+	string cad = cadenaDeDatos;
+	int longitudCadena = cad.length();
+	int espacioOcupado = obtenerTamanioLibro(cadenaDeDatos);
+	espacioOcupado += longitudCadena;
+	fh.seekg(offset);
+	cout << "Estoy parado en : " << fh.tellg() << endl;
+	cout << "Y debo leer una cadena de :" << espacioOcupado << endl;
+	char * libroLeido = (char*)calloc (espacioOcupado, sizeof(char));
+	free(libroLeido);
+	fh.write(libroLeido, espacioOcupado);
+	fh.flush();
+	fh.close();
 }
