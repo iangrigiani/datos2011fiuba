cb29cc778ae7a085d3c47e98ece5eea282880365
lucas.uccello <lucas.uccello@ea5e0ef0-e43b-b66d-2e18-010731ec5258>
2011-04-05 00:22:30 +0000

    Subo parser con match de palabras terminado andando y metodo que obtiene la lista de stopwords, solo le falta el verificar si es un stopWords y esta terminado andando.

--- tpDatos2011/src/ParserLibros/ParserDeLibros.cpp
+++ tpDatos2011/src/ParserLibros/ParserDeLibros.cpp
@@ -1,37 +1,38 @@
 #include "ParserDeLibros.h"
 
 ParserDeLibros::ParserDeLibros() {
+	obtenerListaStopWords();
 }
 ParserDeLibros::~ParserDeLibros() {
         delete this->libroActual;
 }
 
-char * ParserDeLibros::getAutor(){
+char * ParserDeLibros::obtenerAutor(){
         char AutorPatron[] = "Autor:\\s*([^\n\r\t]*)";
-        return getFirstMatch(this->libroActual, AutorPatron);
+        return obtenerPrimerMatch(this->libroActual, AutorPatron);
 }
 
-char * ParserDeLibros::getTitulo(){
+char * ParserDeLibros::obtenerTitulo(){
         char TituloPatron[] = "Titulo:\\s*([^\n\r\t]*)";
-        return getFirstMatch(this->libroActual, TituloPatron);
+        return obtenerPrimerMatch(this->libroActual, TituloPatron);
 }
 
-char * ParserDeLibros::getEditorial(){
-        char EditorialtPatron[] = "Editorial:\\s*([^\n]*)";
-        return getFirstMatch(this->libroActual, EditorialtPatron);
+char * ParserDeLibros::obtenerEditorial(){
+        char EditorialtPatron[] = "Editorial:\\s*([^\n\r\t]*)";
+        return obtenerPrimerMatch(this->libroActual, EditorialtPatron);
 }
 
-char * ParserDeLibros::getPalabras(){
-        char PalabrasPatron[]= "Palabras:\\s*([^\r\t]*)\FINPALABRA";
-        return getFirstMatch(this->libroActual, PalabrasPatron);
+char * ParserDeLibros::obtenerPalabras(){
+        char PalabrasPatron[]= "Palabras:\\s*([^FINPALABRA/]*)";
+        return obtenerPrimerMatch(this->libroActual, PalabrasPatron);
 }
 
-char * ParserDeLibros::getFirstMatch(char * string, char *Patron) {
+char * ParserDeLibros::obtenerPrimerMatch(char * string, char *patronDeFiltro) {
         regex_t preg;
         regmatch_t pmatch[2];
-        char pom[STRING_BUFFER];
+        char* pom = (char*)calloc(STRING_BUFFER, sizeof(char));
         /* it's possible something won't compile like  ./regexp '*' abc */
-        regcomp (&preg, Patron, REG_EXTENDED);
+        regcomp (&preg, patronDeFiltro, REG_EXTENDED);
         regexec (&preg, string, 2, pmatch, 0);
         strncpy (pom, string + pmatch[1].rm_so,
                         pmatch[1].rm_eo - pmatch[1].rm_so);
@@ -41,16 +42,13 @@ char * ParserDeLibros::getFirstMatch(char * string, char *Patron) {
 }
 
 Registro * ParserDeLibros::obtenerRegistroDeLibro (char * libro){
-        /*
-         * TODO: Aca iría la logica para las palabras(Downcase y Stopwords)
-         */
         this->libroActual = libro;
         Registro* nuevoRegistro = new Registro();
-        nuevoRegistro->setAutor(this->getAutor());
-        nuevoRegistro->setEditorial(this->getEditorial());
-        nuevoRegistro->setTitulo(this->getTitulo());
+        nuevoRegistro->setAutor(this->obtenerAutor());
+        nuevoRegistro->setEditorial(this->obtenerEditorial());
+        nuevoRegistro->setTitulo(this->obtenerTitulo());
 
-        char * str = this->getPalabras();
+        char * str = this->obtenerPalabras();
         char * pch;
 		pch = strtok (str," \n");
 		list<string> listaAux;
@@ -58,8 +56,43 @@ Registro * ParserDeLibros::obtenerRegistroDeLibro (char * libro){
 		while (pch != NULL)
 		{
 			pch = strtok (NULL, " \n");
-			if (pch) listaAux.push_back(pch);
+			if (pch) {
+				if (!esStopWords(pch)){
+					listaAux.push_back(caseFolding(pch));
+				}
+			}
 		}
 		nuevoRegistro->setPalabras(listaAux);
         return nuevoRegistro;
+}
+
+bool ParserDeLibros::esStopWords(char* pch){
+	// TODO verificar si es stopWords con busqueda en la lista
+	return false;
+}
+
+string ParserDeLibros::caseFolding(char* pch){
+	// TODO Aplicar el case folding a la palabra antes de almacenarla
+	// habria que ver si esto se aplica a todos los parametros y no solo
+	// a las palabras, sino que tambien, autor, editorial, etc.
+	return pch;
+}
+
+void ParserDeLibros::obtenerListaStopWords(){
+	ifstream fd;
+	fd.open(PATH_STOPWORDS, ios_base::in);
+	fd.seekg(0, ios_base::end);
+	int tamanio = fd.tellg();
+	fd.seekg(0, ios_base::beg);
+	char* lista = (char*)calloc(tamanio, sizeof(char));
+	fd.read(lista, tamanio);
+	char * pch;
+	pch = strtok (lista,"\n");
+	this->listaStopWords.push_back(pch);
+	while (pch != NULL)
+	{
+		pch = strtok (NULL, "\n");
+		if (pch) this->listaStopWords.push_back(pch);
+	}
+	fd.close();
 }
