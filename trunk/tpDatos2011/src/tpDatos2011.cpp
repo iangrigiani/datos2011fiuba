7502323be537a7c3d3e885fc11ac3449a0089675
lucas.uccello <lucas.uccello@ea5e0ef0-e43b-b66d-2e18-010731ec5258>
2011-04-08 14:40:54 +0000

    Modifico el eliminar de EL y el insertar Registro todo sobre el Handler RLV

--- trunk/tpDatos2011/src/tpDatos2011.cpp
+++ trunk/tpDatos2011/src/tpDatos2011.cpp
@@ -14,23 +14,23 @@ using namespace std;
 #include "ArbolB+/ArbolBMas.h"
 
 int main() {
-	ParserDeLibros* p = new ParserDeLibros();
-	ifstream fd;
-	fd.open("Test1.txt", ios_base::in);
-	fd.seekg(0, ios_base::end);
-	int tamanio = fd.tellg();
-	fd.seekg(0, ios_base::beg);
-	char* libro = (char*)calloc(tamanio, sizeof(char));
-	fd.read(libro, tamanio);
-	fd.close();
-	cout << "El libro es: " << endl;
-	cout << libro << endl;
-	Registro* reg = p->obtenerRegistroDeLibro(libro);
-	reg->toString();
-	p->mostrarLista();
-//	int offset = handler->insertarRegistro("Test1.txt");
-//	cout << "Grabe libro en offset: " << offset <<  endl;
-	delete p;
+//	ParserDeLibros* p = new ParserDeLibros();
+//	ifstream fd;
+//	fd.open("Test1.txt", ios_base::in);
+//	fd.seekg(0, ios_base::end);
+//	int tamanio = fd.tellg();
+//	fd.seekg(0, ios_base::beg);
+//	char* libro = (char*)calloc(tamanio, sizeof(char));
+//	fd.read(libro, tamanio);
+//	fd.close();
+//	cout << "El libro es: " << endl;
+//	cout << libro << endl;
+//	Registro* reg = p->obtenerRegistroDeLibro(libro);
+//	reg->toString();
+//	p->mostrarLista();
+////	int offset = handler->insertarRegistro("Test1.txt");
+////	cout << "Grabe libro en offset: " << offset <<  endl;
+//	delete p;
 //	HandlerArchivoRLV* handler2 = new HandlerArchivoRLV();
 //	handler2->insertarLibro("Libro.txt");
 //	cout << "Grabe libro" << endl;
@@ -44,8 +44,12 @@ int main() {
 //			cout << "Grabe Libro" << endl;
 //	}
 //	delete handler;
-//	HandlerArchivoRLV* handler2 = new HandlerArchivoRLV();
-//	char * Libro = handler2->buscarLibro(0);
-//	cout << Libro;
-//	delete handler2;
+	HandlerArchivoRLV* handler2 = new HandlerArchivoRLV();
+	int offset = handler2->insertarRegistro("Test1.txt");
+	cout << "inserte en el offset: " << offset << endl;
+
+//	handler2->quitarRegistro(219);
+//	handler2->quitarRegistro(440);
+//	cout << "Elimine Registros" << endl;
+	delete handler2;
 }
