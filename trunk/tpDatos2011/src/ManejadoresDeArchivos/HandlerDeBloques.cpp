d2165bd4835404ffa9ca7854d2b3e405a0f0de15
lucas.uccello <lucas.uccello@ea5e0ef0-e43b-b66d-2e18-010731ec5258>
2011-04-14 14:41:49 +0000

    cosillas

--- trunk/tpDatos2011/src/ManejadoresDeArchivos/HandlerDeBloques.cpp
+++ trunk/tpDatos2011/src/ManejadoresDeArchivos/HandlerDeBloques.cpp
@@ -5,15 +5,11 @@ HandlerDeBloques::HandlerDeBloques() {
 
 HandlerDeBloques::HandlerDeBloques(string path){
 	this->path = path;
-	if (!ff.is_open()){
-		ff.open(this->path.c_str(), std::ios_base::in | std::ios_base::out);
-	}else{
-		ff.close();
-		ff.open(this->path.c_str() , std::ios_base::in | std::ios_base::out);
-	}
 }
 
 void HandlerDeBloques::guardar_bloque(char* buffer, int nro_bloque){
+	fstream ff;
+	ff.open(this->path.c_str(), std::ios_base::in | std::ios_base::out);
 	int offset_bloque = nro_bloque * TAMANIO_BUFFER;
 // Manejo sobre el archivo de bloques
 	stringstream ss;
@@ -22,29 +18,38 @@ void HandlerDeBloques::guardar_bloque(char* buffer, int nro_bloque){
 	// Escribo libro en el archivo de bloques
 	ff.seekg(offset_bloque);
 	ff.write(str.c_str(), str.length());
+	ff.flush();
+	ff.close();
 }
 
-char* HandlerDeBloques::recuperar_bloque(int nro_bloque)
-{
+char* HandlerDeBloques::recuperar_bloque(int nro_bloque){
+	fstream ff;
+	ff.open(this->path.c_str(), std::ios_base::in | std::ios_base::out);
 	char * bloqueARetornoar = (char*)calloc(TAMANIO_BUFFER, sizeof(char));
 	int offset_bloque = nro_bloque * TAMANIO_BUFFER;
 	// Me posiciono en el archivo de RLV
 	ff.seekg(offset_bloque);
 	ff.read(bloqueARetornoar, TAMANIO_BUFFER);
+	ff.close();
 	string cad = bloqueARetornoar;
 	return ((char*)cad.c_str());
 }
 
 int HandlerDeBloques::get_tam_arch_bloques(){
+	fstream ff;
+	ff.open(this->path.c_str(), std::ios_base::in | std::ios_base::out);
 	ff.seekg(0, ios_base::beg);
 	ff.seekg(0, ios_base::end);
 	int tamanio = ff.tellg();
 	ff.seekg(0, ios_base::beg);
+	ff.close();
 	return tamanio;
 }
 
 void HandlerDeBloques::eliminar_bloque(int nro_Bloque){
+	fstream ff;
 // Abro el archivo y me posiciono para obtener los datos
+	ff.open(this->path.c_str(), std::ios_base::in | std::ios_base::out);
 	int offset_bloque = nro_Bloque * TAMANIO_BUFFER;
 	char * bloqueABorrar = (char*)calloc(TAMANIO_BUFFER, sizeof(char));
 	ff.seekg(offset_bloque);
@@ -54,4 +59,5 @@ void HandlerDeBloques::eliminar_bloque(int nro_Bloque){
 	string borrar = oss.str();
 	ff.write(borrar.c_str(), borrar.length());
 	ff.flush();
+	ff.close();
 }
