dce922be13d6a64592df4a5320d6ef8df9846b1b
andrecartman@gmail.com <andrecartman@gmail.com@ea5e0ef0-e43b-b66d-2e18-010731ec5258>
2011-04-23 02:05:54 +0000

    HandlerArchivoLog terminado (probado y funcionando =P )

--- trunk/tpDatos2011/src/ManejadoresDeArchivos/HandlerArchivoLog.cpp
+++ trunk/tpDatos2011/src/ManejadoresDeArchivos/HandlerArchivoLog.cpp
@@ -15,6 +15,7 @@
 #include <string.h>
 #include "../EstructurasEnComun/Constantes.h"
 
+
 HandlerArchivoLog::HandlerArchivoLog() {
 	// TODO Auto-generated constructor stub
 }
@@ -24,25 +25,80 @@ HandlerArchivoLog::~HandlerArchivoLog() {
 }
 
 void HandlerArchivoLog::insertarRegistro(int IDArchivo){
-	string linea = crearStringAInsertar(IDArchivo);
+	string linea = crearStringAInsertar(IDArchivo, 0, 0, 0, 0);
 	ofstream archivoLog;
 	archivoLog.open(PATH_ARCHIVO_LOG,std::ios_base::app);
 	archivoLog.write(linea.c_str(), linea.length());
 	archivoLog.close();
 }
 
-void HandlerArchivoLog::buscarRegistro(int IDArchivo){}
-void HandlerArchivoLog::setearIndexadoPorAutor(int IDArchivo){}
-void HandlerArchivoLog::setearIndexadoPorEditorial(int IDArchivo){}
-void HandlerArchivoLog::setearIndexadoPorTitulo(int IDArchivo){}
-void HandlerArchivoLog::setearIndexadoPorPalabras(int IDArchivo){}
+int HandlerArchivoLog::buscarRegistro(int IDArchivo){
 
+    bool registroEncontrado = false ;
+	char* cadena = (char*)calloc(100, sizeof(char));
+    string cad;
+    int puntero = 0 , IDActual= 0 ;
 
-string HandlerArchivoLog::crearStringAInsertar(int numero){
-   stringstream ss;
-   ss << numero  << "|" << "0" << "|" << "0" << "|" << "0"<< "|" << "0" << "\n" ;
-   return ss.str();
+    fstream archivoLog;
+	archivoLog.open(PATH_ARCHIVO_LOG, std::ios_base::in | std::ios_base::out);
+    archivoLog.seekg(0);
+
+	while (!registroEncontrado && !archivoLog.eof() ){
+        puntero = archivoLog.tellg();
+		archivoLog.getline (cadena, 100);
+        cad = cadena;
+        if (cad.length() > 0 ) {
+            IDActual= atoi(strtok(cadena,"|"));
+            if (IDActual == IDArchivo){
+                registroEncontrado = true;
+            }
+	    }
+    }
+	archivoLog.close();
+	if(!registroEncontrado){
+		puntero = -1;
+	}
+    return puntero;
 }
 
 
+void HandlerArchivoLog::setearIndexado(int IDArchivo, int parametro){
+    int puntero = buscarRegistro(IDArchivo);
+    int ID= 0, ind1=0 , ind2=0, ind3=0, ind4=0;
+    char* cadena = (char*)calloc(100, sizeof(char));
+    string linea;
 
+    fstream archivoLog;
+	archivoLog.open(PATH_ARCHIVO_LOG, std::ios_base::in | std::ios_base::out);
+    archivoLog.seekg(puntero);
+    archivoLog.getline(cadena,100);
+
+    ID = atoi(strtok(cadena,"|"));
+    ind1 = atoi ( strtok(NULL,"|") );
+    ind2 = atoi ( strtok(NULL,"|") );
+    ind3 = atoi ( strtok(NULL,"|") );
+    ind4 = atoi ( strtok(NULL,"\n") );
+
+    switch (parametro){
+        case 'a':{ ind1 = 1; break;}
+
+        case 'e':{ ind2 = 1; break;}
+
+        case 't':{ ind3 = 1; break;}
+
+        case 'p':{ ind4 = 1; break;}
+
+    }
+    linea = crearStringAInsertar(IDArchivo, ind1, ind2, ind3, ind4);
+    archivoLog.seekp(puntero);
+	archivoLog.write(linea.c_str(), linea.length());
+    archivoLog.close();
+
+}
+
+
+string HandlerArchivoLog::crearStringAInsertar(int numero, int ind1, int ind2, int ind3, int ind4){
+   stringstream ss;
+   ss << numero  << "|" << ind1 << "|" << ind2 << "|" << ind3 << "|" << ind4 << "\n"  ;
+   return ss.str();
+}
