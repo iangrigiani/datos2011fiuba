26033bacbe6e33ecd60da9755e07cf57fafd0636
andrecartman <andrecartman@gmail.com>
2011-05-07 05:20:52 +0000

    Finalizados TestParserDeLibros. Encontré un pequeño bug al recuperar la lista de palabras: Si aparece un signo - se reemplaza por una z como si fuera un caracter raro. Debería hacer una excepción y eliminar dicho caracter de la palabra que guarda la lista del registro.

--- tpDatos2011/test/TestParserDeLibros.h
+++ tpDatos2011/test/TestParserDeLibros.h
@@ -2,17 +2,26 @@
 #define TESTPARSERDELIBROS_H
 
 #include <cppunit/extensions/HelperMacros.h>
+#include "../src/ParserLibros/ParserDeLibros.h"
 
-class TestParserDeLibros : public CppUnit::TestFixture
-{
-  CPPUNIT_TEST_SUITE(TestParserDeLibros);
-  CPPUNIT_TEST(testConstructor);
-  CPPUNIT_TEST_SUITE_END();
-
+class TestParserDeLibros : public CppUnit::TestFixture{
 public:
+  ParserDeLibros* parser;
   void setUp();
   void tearDown();
-  void testConstructor();
+  void testObtenerAutorEditorialTituloDeLibroConTodosLosDatos();
+  void testObtenerPalabrasConTodosLosDatos();
+  void testObtenerEditorialEnLibroSinEditorial();
+  CPPUNIT_TEST_SUITE(TestParserDeLibros);
+  CPPUNIT_TEST(testObtenerAutorEditorialTituloDeLibroConTodosLosDatos);
+  CPPUNIT_TEST(testObtenerPalabrasConTodosLosDatos);
+  CPPUNIT_TEST(testObtenerEditorialEnLibroSinEditorial);
+  CPPUNIT_TEST_SUITE_END();
+private:
+  string generarMockLibroCompleto();
+  string generarMockLibroSinEditorial();
+  void	 generarListaPatron(list<string>& listaDeIds);
+
 };
 
 #endif  // TESTPARSERDELIBROS_H
